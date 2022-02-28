4e2de0546b3ca19d2571ec30784a482e90b57382
Pnikanti <patrick.nikanti@swtech.fi>
2022-02-28 14:26:14 +0200

    add mat4. WIP

--- src/gmath/gmath.h
+++ src/gmath/gmath.h
@@ -559,7 +559,7 @@ struct mat2
 
 	vec2<T> operator[](int index) const
 	{
-		OUTOFBOUNDSCHECK(sizeof(T) * index, bounds / 2);
+		OUTOFBOUNDSCHECK(sizeof(T) * index, bounds / 4);
 		return _v[index];
 	}
 
@@ -672,7 +672,7 @@ struct mat3
 
 	vec3<T> operator[](int index) const
 	{
-		OUTOFBOUNDSCHECK(sizeof(T) * index, bounds / 3);
+		OUTOFBOUNDSCHECK(sizeof(T) * index, bounds / 9);
 		return _v[index];
 	}
 
@@ -695,7 +695,135 @@ struct mat3
 		vec3<T> _v[3];
 	};
 
-	int bounds = sizeof(T) * 6;
+	int bounds = sizeof(T) * 9;
+};
+
+template <typename T>
+struct mat4
+{
+	// Constructors
+
+	mat4(
+		T x1, T y1, T z1, T w1,
+		T x2, T y2, T z2, T w2,
+		T x3, T y3, T z3, T w3,
+		T x4, T y4, T z4, T w4
+	) : x(x1, x2, x3, x4), y(y1, y2, y3, y4), z(z1, z2, z3, z4) {}
+
+	mat4(vec4<T> _x, vec4<T> _y, vec4<T> _z) : x(_x), y(_y), z(_z) {}
+
+	mat4(const mat4<T>& a) : x(a.x), y(a.y), z(a.z), w(a.w) {}
+
+	// Scalar operation
+	mat4(const T& a) : x(a), y(a), z(a), w(a) {}
+
+
+	// Operator overloads
+
+	mat4<T> operator+ (const mat4<T>& a) const
+	{
+		return mat4<T>(
+			x.x + a.x.x, y.x + a.y.x, z.x + a.z.x, w.x + a.w.x,
+			x.y + a.x.y, y.y + a.y.y, z.y + a.z.y, w.y + a.w.y,
+			x.z + a.x.z, y.z + a.y.z, z.z + a.z.z, w.z + a.w.z,
+			x.w + a.x.w, w.z + a.y.w, z.w + a.z.w, w.w + a.w.w
+		);
+	}
+
+	// Scalar operation
+	mat4<T> operator+ (const T& a) const
+	{
+		return mat4<T>(
+			x.x + a, y.x + a, z.x + a, w.x + a,
+			x.y + a, y.y + a, z.y + a, w.y + a,
+			x.z + a, y.z + a, z.z + a, w.z + a,
+			x.w + a, y.w + a, z.w + a, w.w + a
+		);
+	}
+
+	mat4<T> operator- (const mat4<T>& a) const
+	{
+		return mat4<T>(
+			x.x - a.x.x, y.x - a.y.x, z.x - a.z.x, w.x - a.w.x,
+			x.y - a.x.y, y.y - a.y.y, z.y - a.z.y, w.y - a.w.y,
+			x.z - a.x.z, y.z - a.y.z, z.z - a.z.z, w.z - a.w.z,
+			x.w - a.x.w, w.z - a.y.w, z.w - a.z.w, w.w - a.w.w
+		);
+	}
+
+	// Scalar operation
+	mat4<T> operator- (const T& a) const
+	{
+		return mat4<T>(
+			x.x - a, y.x - a, z.x - a, w.x - a,
+			x.y - a, y.y - a, z.y - a, w.y - a,
+			x.z - a, y.z - a, z.z - a, w.z - a,
+			x.w - a, y.w - a, z.w - a, w.w - a
+		);
+	}
+
+	mat4<T> operator* (const mat4<T>& a) const
+	{
+		// Column major product
+
+		return mat4<T>(
+			(x.x * a.x.x + x.y * a.y.x + x.z * a.z.x + x.w * a.w.x), (y.x * a.x.x + y.y * a.y.x + y.z * a.z.x + y.w * a.w.x), (z.x * a.x.x + z.y * a.y.x + z.z * a.z.x + z.w * a.w.x), (w.x * a.x.x + w.y * a.y.x + w.z * a.z.x + w.w * a.w.x)
+			(x.x * a.x.y + x.y * a.y.y + x.z * a.z.y + x.w * a.w.y), (y.x * a.x.y + y.y * a.y.y + y.z * a.z.y + y.w * a.w.y), (z.x * a.x.y + z.y * a.y.y + z.z * a.z.y + z.w * a.w.y), (w.x * a.x.y + w.y * a.y.y + w.z * a.z.y + w.w * a.w.y)
+			(x.x * a.x.z + x.y * a.y.z + x.z * a.z.z + x.w * a.w.z), (y.x * a.x.z + y.y * a.y.z + y.z * a.z.z + y.w * a.w.z), (z.x * a.x.z + z.y * a.y.z + z.z * a.z.z + z.w * a.w.z), (w.x * a.x.z + w.y * a.y.z + w.z * a.z.z + w.w * a.w.z)
+			(x.x * a.x.w + x.y * a.y.w + x.z * a.z.w + x.w * a.w.w), (y.x * a.x.w + y.y * a.y.w + y.z * a.z.w + y.w * a.w.w), (z.x * a.x.w + z.y * a.y.w + z.z * a.z.w + z.w * a.w.w), (w.x * a.x.w + w.y * a.y.w + w.z * a.z.w + w.w * a.w.w)
+		);
+	}
+
+	// Scalar operation
+	mat4<T> operator* (const T& a) const
+	{
+		// Column major product
+
+		return mat4<T>(
+			x.x * a, y.x * a, z.x * a, w.x * a,
+			x.y * a, y.y * a, z.y * a, w.y * a,
+			x.z * a, y.z * a, z.z * a, w.z * a,
+			x.w * a, y.w * a, z.w * a, w.w * a
+		);
+	}
+
+	// Scalar operation
+	mat4<T> operator/ (const T& a) const
+	{
+		return mat4<T>(
+			x.x / a, y.x / a, z.x / a, w.x / a,
+			x.y / a, y.y / a, z.y / a, w.y / a,
+			x.z / a, y.z / a, z.z / a, w.z / a,
+			x.w / a, y.w / a, z.w / a, w.w / a
+		);
+	}
+
+	vec4<T> operator[](int index) const
+	{
+		OUTOFBOUNDSCHECK(sizeof(T) * index, bounds / 16);
+		return _v[index];
+	}
+
+
+	bool operator== (const mat4<T>& a) const
+	{
+		return (x == a.x) && (y == a.y) && (z == a.z) && (w == a.w);
+	}
+
+
+	friend std::ostream& operator<< (std::ostream& stream, const mat4<T>& a)
+	{
+		return stream << "[" << a.x << ", " << a.y << ", " << a.z << ", " << a.w << "]";
+	}
+
+	// Attributes
+
+	union {
+		struct { vec4<T> x, y, z, w; };
+		vec4<T> _v[4];
+	};
+
+	int bounds = sizeof(T) * 16;
 };
 
 typedef mat2<int> mat2i;
@@ -705,6 +833,10 @@ typedef mat2<double> mat2d;
 typedef mat3<int> mat3i;
 typedef mat3<float> mat3f;
 typedef mat3<double> mat3d;
+
+typedef mat4<int> mat4i;
+typedef mat4<float> mat4f;
+typedef mat4<double> mat4d;
 
 struct Eangles
 {
